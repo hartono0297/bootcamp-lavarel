b255de9e6a00f136abff09aac8c2649db1d5b634
Hartono <hartonoaka97@gmail.com>
2022-03-27 15:11:22 +0700

    update request 50%

---
+++ app/Http/Requests/User/UpdateUserRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use App\Models\User;
+//use Gate;
+use Illuminate\Foundation\Http\FormRequest;
+use Symfony\Component\HttpFoundation\Response;
+
+use illuminate\validation\rules;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => [
+                'required', 'string', 'max:255',
+            ],
+            'email' => [
+                'required', 'email', 'max:255', Rule::unique('users')->ignore($this->user),
+                // rule unique only work for other record id
+            ],
+            'password' => [
+                'min:8', 'string', 'max:255', 'mixedCase',
+            ],
+            // add validation for role this here
+        ];
+    }
+}
