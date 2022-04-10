2d9fdb523f3cc3a5f823263b8f9456b94f213b03
Hartono <hartonoaka97@gmail.com>
2022-04-10 15:12:32 +0700

    10 maret

--- resources/views/pages/backsite/dashboard/index.blade.php
+++ resources/views/pages/backsite/dashboard/index.blade.php
@@ -1,1 +1,16 @@
-Home
+@extends('layouts.app')
+
+@section('title', 'Dashboard')
+
+@section('content')
+
+<!--begin content-->
+<div class="app-content content">
+        <div class="content-overlay"></div>
+        <div class="content-wrapper">
+            HOME
+    </div>
+</div>
+<!--end content-->
+
+@endsection
